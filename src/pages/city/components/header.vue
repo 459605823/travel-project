90fc1bd1a9e4967c8872d7edf0dbd56ef487a1a0
beast <459605823@qq.com>
2018-11-16 15:33:01 +0800

    city router

---
+++ src/pages/city/components/header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+  .header
+    position: relative
+    overflow: hidden
+    height: $headerHeight
+    line-height: $headerHeight
+    font-size: .32rem
+    text-align: center
+    color: #fff
+    background: $bgcolor
+    .header-back
+      color: #fff
+      position: absolute
+      top: 0
+      left:0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+</style>
