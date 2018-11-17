4850b9a88c34568fd89a87929c78cd2cd7f077e9
beast <459605823@qq.com>
2018-11-17 10:29:28 +0800

    finish search

--- src/pages/city/components/search.vue
+++ src/pages/city/components/search.vue
@@ -1,15 +1,56 @@
 <template>
-  <div class="search">
-    <input type="text" class="search-input" placeholder="输入城市名或拼音" @blur="handleInputBlur">
+  <div>
+    <div class="search">
+      <input type="text" class="search-input" placeholder="输入城市名或拼音" v-model="keyword">
+    </div>
+    <div class="search-content" v-show="keyword" ref="search">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="noMatch">没有找到相应城市</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
   name: 'citySearch',
-  methods: {
-    handleInputBlur (e) {
-      console.log(e.target.value)
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    noMatch () {
+      return !this.list.length
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        // 将所有匹配到的城市添加到结果数组中，重新渲染列表
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
     }
   }
 }
@@ -30,4 +71,18 @@ export default {
       color: #666
       border-radius: .06rem
       padding: 0 .1rem
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: #eee
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
+      color: #666
+      background: #fff
 </style>
